66145d27d047ed72b0c80b7dd3e5a4fbf0ff8425
Takanori Matsumoto <matscube@gmail.com>
2024-08-15 15:50:53 +0900

    feat(package.json): add @radix-ui/react-separator dependency for UI separation
    feat(App.tsx): integrate Separator component for better UI structure
    
    style(App.tsx): adjust container and layout classes for header and main
    
    feat(components): add Separator component for consistent UI separation

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Separator } from '@radix-ui/react-select';
 import clsx from 'clsx';
 import { FormMainMaterialSelect } from './components/form-main-material-select';
 import { ModeToggle } from './components/mode-toggle';
@@ -7,15 +8,17 @@ function App() {
   return (
     <>
       <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
-        <div className={clsx('max-w-7xl min-h-screen', 'flex flex-col')}>
-          <header className={clsx('w-full', 'px-4 py-2', 'flex justify-between items-center')}>
+        <div className={clsx('w-full min-h-screen', 'flex flex-col')}>
+          <header
+            className={clsx('w-full container', 'px-4 py-2', 'flex justify-between items-center')}
+          >
             <h1 className={clsx('p-4', 'text-3xl font-bold')}>Blacksmith Simulator</h1>
             <ModeToggle />
           </header>
-          <main className={clsx('px-4 py-2')}>
+          <Separator className='w-full border-b border-gray-800' />{' '}
+          <main className={clsx('container px-4 py-2')}>
             <FormMainMaterialSelect className='p-4' />
           </main>
-
           <Footer />
         </div>
       </ThemeProvider>
@@ -27,7 +30,7 @@ export default App;
 
 const Footer = () => {
   return (
-    <footer className={clsx('mt-auto', 'bg-gray-800 text-white py-6')}>
+    <footer className={clsx('w-full mt-auto', 'bg-gray-800 text-white py-6')}>
       <div className='container mx-auto px-4'>
         <div className='flex flex-col md:flex-row justify-between items-center'>
           <div className='mb-4 md:mb-0'>
