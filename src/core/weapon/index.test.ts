70503218cb8981fb96de2674933449e96afcce1b
Takanori Matsumoto <matscube@gmail.com>
2024-09-13 01:34:27 +0900

    test(weapon): add unit tests for Jinn, Undine, Aura, and Dryad essences to ensure correct tempering behavior and energy limits

--- src/core/weapon/index.test.ts
+++ src/core/weapon/index.test.ts
@@ -42,4 +42,56 @@ describe(`'Weapon's essence of`, () => {
     w.temper("Gnome's Gold");
     expect(w.essence.gnome).toEqual(4);
   });
+
+  it('Jinn can be changed', () => {
+    const w = new Weapon('MenosBronze', 'Knife');
+    w.temper("Jinn's Silver");
+    expect(w.essence.jinn).toEqual(1);
+    w.temper("Jinn's Silver");
+    expect(w.essence.jinn).toEqual(3);
+    w.temper("Jinn's Gold");
+    expect(w.essence.jinn).toEqual(4);
+    // not enough energy
+    w.temper("Jinn's Gold");
+    expect(w.essence.jinn).toEqual(4);
+  });
+
+  it('Undine can be changed', () => {
+    const w = new Weapon('MenosBronze', 'Knife');
+    w.temper("Undine's Silver");
+    expect(w.essence.undine).toEqual(1);
+    w.temper("Undine's Silver");
+    expect(w.essence.undine).toEqual(3);
+    w.temper("Undine's Gold");
+    expect(w.essence.undine).toEqual(4);
+    // not enough energy
+    w.temper("Undine's Gold");
+    expect(w.essence.undine).toEqual(4);
+  });
+
+  it('Aura can be changed', () => {
+    const w = new Weapon('MenosBronze', 'Knife');
+    w.temper("Aura's Silver");
+    expect(w.essence.aura).toEqual(1);
+    w.temper("Aura's Silver");
+    expect(w.essence.aura).toEqual(3);
+    w.temper("Aura's Gold");
+    expect(w.essence.aura).toEqual(4);
+    // not enough energy
+    w.temper("Aura's Gold");
+    expect(w.essence.aura).toEqual(4);
+  });
+
+  it('Dryad can be changed', () => {
+    const w = new Weapon('MenosBronze', 'Knife');
+    w.temper("Dryad's Silver");
+    expect(w.essence.dryad).toEqual(1);
+    w.temper("Dryad's Silver");
+    expect(w.essence.dryad).toEqual(3);
+    w.temper("Dryad's Gold");
+    expect(w.essence.dryad).toEqual(4);
+    // not enough energy
+    w.temper("Dryad's Gold");
+    expect(w.essence.dryad).toEqual(4);
+  });
 });
